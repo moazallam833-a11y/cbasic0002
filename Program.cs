dbf0da32b3e28201222edd71d9be61019c2bb1a1
Moaz Allam <moazalisoliman5@gmail.com>
2026-09-10 01:18:56 +0300

    Q4

--- Program.cs
+++ Program.cs
@@ -34,6 +34,22 @@ namespace c_0002
             Console.WriteLine(sb);
 
             #endregion
+            #region Q8
+            // Given int pages = 464;, write an if / else statement that prints
+            // "Long Book" if pages is greater than 300, otherwise prints "Short Book".
+
+            int pages4 = 464;
+
+            if (pages4 > 300)
+            {
+                Console.WriteLine("Long Book");
+            }
+            else
+            {
+                Console.WriteLine("Short Book");
+            }
+            #endregion
+
 
 
 
