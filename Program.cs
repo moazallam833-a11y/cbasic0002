f0aba72a71f8d504173c72242878a4859487fedd
Moaz Allam <moazalisoliman5@gmail.com>
2026-09-10 01:23:04 +0300

    Q17

--- Program.cs
+++ Program.cs
@@ -169,6 +169,24 @@ namespace c_0002
             }
             #endregion
 
+            #region Q17
+            // Using the same books array, print every title except
+            // "The Pragmatic Programmer" (skip it with continue, don't stop the loop).
+
+            string[] books5 = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+
+            foreach (string book in books5)
+            {
+                if (book == "The Pragmatic Programmer")
+                {
+                    continue;
+                }
+
+                Console.WriteLine(book);
+            }
+            #endregion
+
+
 
 
 
